792ac6b0154dafc666fa86941041ece4e9a85f42
Alek Slater <alek@davincium.com>
2016-12-07 11:27:51 +0800

    Update DropboxFileProvider.swift
    Exposes the overwrite parameter for writeContents in DropboxFileProvider

--- Sources/DropboxFileProvider.swift
+++ Sources/DropboxFileProvider.swift
@@ -252,12 +252,16 @@ extension DropboxFileProvider: FileProviderReadWrite {
     }
     
     public func writeContents(path: String, contents data: Data, atomically: Bool = false, completionHandler: SimpleCompletionHandler) -> OperationHandle? {
+        return writeContents(path:path,contents:data,atomically:atomically,overwrite:true,completionHandler:completionHandler)
+    }
+    
+    public func writeContents(path: String, contents data: Data, atomically: Bool = false, overwrite:Bool, completionHandler: SimpleCompletionHandler) -> OperationHandle? {
         let opType = FileOperationType.modify(path: path)
         guard fileOperationDelegate?.fileProvider(self, shouldDoOperation: opType) ?? true == true else {
             return nil
         }
         // FIXME: remove 150MB restriction
-        return upload_simple(path, data: data, overwrite: true, operation: opType, completionHandler: completionHandler)
+        return upload_simple(path, data: data, overwrite: overwrite, operation: opType, completionHandler: completionHandler)
     }
     
     public func searchFiles(path: String, recursive: Bool, query: String, foundItemHandler: ((FileObject) -> Void)?, completionHandler: @escaping ((_ files: [FileObject], _ error: Error?) -> Void)) {
