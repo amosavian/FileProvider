1f03ce6cccdf81d1c3bc5f87560e3aeacd2708e9
Alek Slater <slayte@gmail.com>
2017-03-29 12:51:11 +0800

    potential fix for warning about unused variable in FTPHelper, check with amosavian if this is right

--- Sources/FTPHelper.swift
+++ Sources/FTPHelper.swift
@@ -221,7 +221,7 @@ extension FTPFileProvider {
             }
             
             let command = useMLST ? "MLSD \(path)" : "LIST \(path)"
-            self.execute(command: "MLSD \(path)", on: task, minLength: 70, afterSend: { error in
+            self.execute(command: command, on: task, minLength: 70, afterSend: { error in
                 // starting passive task
                 let timeout = self.session.configuration.timeoutIntervalForRequest
                 let passiveTask = self.session.fpstreamTask(withHostName: host, port: port)
