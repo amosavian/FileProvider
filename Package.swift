3d563c3bfa147f996f72b3477556cdb9f3a61441
Amir Abbas <a.mosavian@gmail.com>
2017-10-27 09:16:48 +0330

    Updated SPM file to Swift 4

--- Package.swift
+++ Package.swift
@@ -1,5 +1,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "FilesProvider"
+    name: "FilesProvider",
+    products: [
+        .library(name: "FilesProvider",  targets: ["FilesProvider"])
+    ],
+    dependencies: [],
+    targets: [
+        .target(name: "FilesProvider", path: "Sources")
+    ]
 )
