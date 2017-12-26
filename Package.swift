05916c09a2ce41daf69672b28bb4e8cdc66d000c
Amir Abbas <a.mosavian@gmail.com>
2017-12-26 11:37:19 +0330

    Fix #74 (SPM issue)

--- Package.swift
+++ Package.swift
@@ -1,12 +1,24 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
     name: "FilesProvider",
     products: [
-        .library(name: "FilesProvider",  targets: ["FilesProvider"])
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "FilesProvider",
+            targets: ["FilesProvider"],
+        )
     ],
     dependencies: [],
     targets: [
-        .target(name: "FilesProvider", path: "Sources")
+        .target(name: "FilesProvider",
+                dependencies: [],
+                path: "Sources"),
+        .target(name: "FilesProviderTests",
+                dependencies: ["FilesProvider"],
+                path: "Tests"),
     ]
 )
